c726830b680e8a2e161a3169a01150b7cf7face8
dodhyk <50976164+dodhyk@users.noreply.github.com>
2020-12-05 18:33:28 +0700

    login

---
+++ ekasir/application/controllers/Login.php
@@ -0,0 +1,17 @@
+<?php
+
+class Login extends CI_Controller
+{
+    public function __construct(){
+        parent::__construct();
+    }
+
+    function index(){
+        $this->load->view('login');
+    }
+}
+
+
+
+
+?>
